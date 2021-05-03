9c248eae2687e4109253c181eb320fcf7c4d58f8
akaysing <adarshk03.singh@gmail.com>
2021-05-04 01:12:03 +0530

    understanding DOM

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const buttonPrimary = document.querySelector("#btn-primary");
+const inputText = document.querySelector("#txt-area");
+
 buttonPrimary.addEventListener("click",function(){
-    console.log("clicked!");
+    console.log(inputText.value);
 })
     
 
