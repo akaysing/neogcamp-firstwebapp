98483f77bf680a2c13da20d1c121ad1b729ea859
akaysing <adarshk03.singh@gmail.com>
2021-05-07 00:05:12 +0530

    added feature to translate to yodish

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const buttonPrimary = document.querySelector("#btn-primary");
 const inputText = document.querySelector("#txt-area");
 const outputText = document.querySelector("#txt-output");
+const serverUrl = "https://api.funtranslations.com/translate/yoda.json";
+var fetchUrl;
 
-buttonPrimary.addEventListener("click",function(){
-    outputText.innerText = inputText.value;
+function translationUrl() {
+    fetchUrl = serverUrl + "?" + "text=" + inputText.value;
+}
+
+buttonPrimary.addEventListener("click", function () {
+    translationUrl();
+    fetch(fetchUrl)
+        .then(response => response.json())
+        .then(json => outputText.innerText = json.contents.translated)
 });
-
-
-    
-
-    
